0ff374f354494d0ad0eb85fd937f25b1c2620e47
username-1-2-3-4-5-6-7 <rimimashallah@gmail.com>
2026-09-23 09:06:47 -0700

    Handle `getAngleDelta`, possibly

--- js/spinning_triangle.js
+++ js/spinning_triangle.js
@@ -6,6 +6,8 @@ function setup() {
 }
 
 function draw() {
+  const angleDelta = getAngleDelta();
+
   background(8, 8, 8, 1);
 
   translate(width / 2, height / 2);
@@ -15,14 +17,14 @@ function draw() {
   let ctx = drawingContext;
 
   let radialGradient = ctx.createRadialGradient(0, 10, 0, 0, 0, 80);
-  radialGradient.addColorStop(0., "red");
+  radialGradient.addColorStop(0, "red");
   radialGradient.addColorStop(0.33, "orange");
   radialGradient.addColorStop(0.66, "yellow");
-  radialGradient.addColorStop(1., "green");
+  radialGradient.addColorStop(1, "green");
 
   ctx.fillStyle = radialGradient;
 
   triangle(0, -80, 70, 60, -70, 60);
 
-  angle += 0.01;
+  angle += angleDelta;
 }
